dac64697df4441b11599ab0893a95cb47d5196a5
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-11-24 20:50:34 +0700

    can start sever & handler '/first-user' request

--- index.js
+++ index.js
@@ -23,3 +23,51 @@ Expected Response
 
 > __Note:__ Use `POSTMAN` for testing the APIs and `nodemon` for fast development
 */
+
+const infoJson = require('./assets/info.json');
+
+//----------
+
+const getFirstUserObject = (isSortAsc) => {
+	const originalUserArray = infoJson.data;
+	const tempUserArray = [...originalUserArray];
+
+	if(isSortAsc) {
+		tempUserArray.sort((obj1, obj2) => {
+			return obj1.name > obj2.name;
+		});
+	}
+    
+	return tempUserArray[0];
+};
+
+const getFirstUserHandler = (request, reply) => {
+	const isSortAsc = (request.query['sort'] === 'true');
+	const firstUser = getFirstUserObject(isSortAsc);
+	reply(firstUser.name);
+};
+
+//----------
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+const host = 'localhost';
+const port = 3008;
+server.connection({port: port, host: host});
+
+server.route({
+    method: 'GET',
+    path: '/first-user',
+    handler: getFirstUserHandler
+});
+
+server.start(() => {
+    console.log('Server started with port', port);
+});
+
+//----------
+
+module.exports = {
+	getFirstUserHandler
+}
